27d562a142a9c0cc477dea3821375fd9de036eb7
cabbage <verasakurali@163.com>
2022-11-07 20:15:20 +0800

    同步

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -2,13 +2,24 @@ module.exports = {
   title: "Wzx’s Blog",
   description: 'A simple a blog.',
   base:'/wzxblog/',
-  dest: 'public',
   head: [
     ['link', { rel: 'icon', href: '/favicon.ico' }],
     ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
   ],
   theme: 'reco',
   themeConfig: {
+    /*用vssue 搞的评论模块，github一直登录不上*/
+    // vssueConfig:{
+    //   platform:'github',
+    //   owner:'linxiafeng',
+    //   repo:'docsComment',
+    //   clientId:'f0fb6d033dd17568050b',
+    //   clientSecret:'4690809ecfc2bdde318a7e15dd06ea1545ffa332'
+    // },
+    valineConfig:{
+      appId: 'igRCCzChgsUjo3k8CMEhT2e2-gzGzoHsz',
+      appKey: 'BwW5PPa8HmkZ4qMx9de2w4Bl'
+    },
     nav: [
       { text: 'Home', link: '/', icon: 'reco-home' },
       { text: 'TimeLine', link: '/timeline/', icon: 'reco-date' },
@@ -21,7 +32,7 @@ module.exports = {
       { text: 'Contact', 
         icon: 'reco-message',
         items: [
-          { text: 'GitHub', link: 'https://github.com/recoluan', icon: 'reco-github' }
+          { text: 'GitHub', link: 'https://github.com/linxiafeng', icon: 'reco-github' }
         ]
       }
     ],
@@ -68,22 +79,22 @@ module.exports = {
     // 最后更新时间
     lastUpdated: 'Last Updated',
     // 作者
-    author: 'Wzx',
+    author: 'cabbage',
     // 作者头像
     authorAvatar: '/avatar.jpg',
     // 备案号
     record: 'xxxx',
     // 项目开始时间
-    startYear: '2022'
+    startYear: '2022',
     /**
      * 密钥 (if your blog is private)
      */
 
-    // keyPage: {
-    //   keys: ['12345'],
-    //   color: '#42b983',
-    //   lineColor: '#42b983'
-    // },
+    keyPage: {
+      keys: ['e17387971e1a876715800d49172d00ca'],
+      color: '#42b983',
+      lineColor: '#42b983'
+    }
 
     /**
      * valine 设置 (if you need valine comment )
